99db02901748155f6eb5a1cce7216663d93f6e48
Rifai-13 <130067475+Rifai-13@users.noreply.github.com>
2024-11-05 21:02:47 +0700

    shope

---
+++ lib/app/modules/shope/views/shope_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/shope_controller.dart';
+
+class ShopeView extends GetView<ShopeController> {
+  const ShopeView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ShopeView'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'ShopeView is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
